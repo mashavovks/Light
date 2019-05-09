4fad85570c854d6481e715135c96c93377515e10
Mariia Vovk <mvovk@icstars.org>
2019-05-09 14:46:36 -0500

    Initial Commit

--- Light1/ViewController.swift
+++ Light1/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
-
+var lightOn = true
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBOutlet weak var lightButton: UIButton!
+    
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        lightOn = !lightOn
+        updateUI()
+       
+    }
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
+            
+        }else {
+            view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
+            
+            
+        }
+    }
+    
 }
 
